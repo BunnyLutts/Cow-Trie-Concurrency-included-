bb74b2f758818258233911c645de52a7afdfaec6
BunnyLutts <lutts@qq.com>
2024-04-01 16:05:34 +0800

    Complete Trie and passed all the tests.

--- trie/src2.hpp
+++ trie/src2.hpp
@@ -1,164 +1,221 @@
 /*
-* Acknowledgement : Yuxuan Wang for Modifying the prototype of TrieStore class 
-*/
+ * Acknowledgement : Yuxuan Wang for Modifying the prototype of TrieStore class
+ */
 
 #ifndef SJTU_TRIE_HPP
 #define SJTU_TRIE_HPP
 
 #include <algorithm>
 #include <cstddef>
+#include <future>
+#include <iostream>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <optional>
+#include <shared_mutex>
 #include <stdexcept>
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <utility>
 #include <vector>
-#include <string_view>
-#include <iostream>
-#include <shared_mutex>
-#include <string_view>
-#include <mutex>  
-#include <future> 
 
 namespace sjtu {
 
-// A TrieNode is a node in a Trie.
-class TrieNode {
- public:
-  // Create a TrieNode with no children.
-  TrieNode() = default;
+    // A TrieNode is a node in a Trie.
+    class TrieNode {
+    public:
+        // Create a TrieNode with no children.
+        TrieNode() = default;
 
-  // Create a TrieNode with some children.
-  explicit TrieNode(std::map<char, std::shared_ptr<const TrieNode>> children) : children_(std::move(children)) {}
+        // Create a TrieNode with some children.
+        explicit TrieNode(std::map<char, std::shared_ptr<const TrieNode>> children) : children_(std::move(children)) {}
 
-  virtual ~TrieNode() = default;
+        virtual ~TrieNode() = default;
 
-  // Clone returns a copy of this TrieNode. If the TrieNode has a value, the value is copied. The return
-  // type of this function is a unique_ptr to a TrieNode.
-  //
-  // You cannot use the copy constructor to clone the node because it doesn't know whether a `TrieNode`
-  // contains a value or not.
-  //
-  // Note: if you want to convert `unique_ptr` into `shared_ptr`, you can use `std::shared_ptr<T>(std::move(ptr))`.
-  virtual auto Clone() const -> std::unique_ptr<TrieNode> { return std::make_unique<TrieNode>(children_); }
+        // Clone returns a copy of this TrieNode. If the TrieNode has a value, the value is copied. The return
+        // type of this function is a unique_ptr to a TrieNode.
+        //
+        // You cannot use the copy constructor to clone the node because it doesn't know whether a `TrieNode`
+        // contains a value or not.
+        //
+        // Note: if you want to convert `unique_ptr` into `shared_ptr`, you can use `std::shared_ptr<T>(std::move(ptr))`.
+        virtual auto Clone() const -> std::unique_ptr<TrieNode> { return std::make_unique<TrieNode>(children_); }
 
-  // A map of children, where the key is the next character in the key, and the value is the next TrieNode.
-  std::map<char, std::shared_ptr<const TrieNode>> children_;
+        // A map of children, where the key is the next character in the key, and the value is the next TrieNode.
+        std::map<char, std::shared_ptr<const TrieNode>> children_;
 
-  // Indicates if the node is the terminal node.
-  bool is_value_node_{false};
+        // Indicates if the node is the terminal node.
+        bool is_value_node_{false};
 
-  // You can add additional fields and methods here. But in general, you don't need to add extra fields to
-  // complete this project.
-};
+        // You can add additional fields and methods here. But in general, you don't need to add extra fields to
+        // complete this project.
+    };
 
-// A TrieNodeWithValue is a TrieNode that also has a value of type T associated with it.
-template <class T>
-class TrieNodeWithValue : public TrieNode {
- public:
-  // Create a trie node with no children and a value.
-  explicit TrieNodeWithValue(std::shared_ptr<T> value) : value_(std::move(value)) { this->is_value_node_ = true; }
+    // A TrieNodeWithValue is a TrieNode that also has a value of type T associated with it.
+    template <class T>
+    class TrieNodeWithValue : public TrieNode {
+    public:
+        // Create a trie node with no children and a value.
+        explicit TrieNodeWithValue(std::shared_ptr<T> value) : value_(std::move(value)) { this->is_value_node_ = true; }
 
-  // Create a trie node with children and a value.
-  TrieNodeWithValue(std::map<char, std::shared_ptr<const TrieNode>> children, std::shared_ptr<T> value)
-      : TrieNode(std::move(children)), value_(std::move(value)) {
-    this->is_value_node_ = true;
-  }
+        // Create a trie node with children and a value.
+        TrieNodeWithValue(std::map<char, std::shared_ptr<const TrieNode>> children, std::shared_ptr<T> value)
+            : TrieNode(std::move(children)), value_(std::move(value)) {
+            this->is_value_node_ = true;
+        }
 
-  // Override the Clone method to also clone the value.
-  //
-  // Note: if you want to convert `unique_ptr` into `shared_ptr`, you can use `std::shared_ptr<T>(std::move(ptr))`.
-  auto Clone() const -> std::unique_ptr<TrieNode> override {
-    return std::make_unique<TrieNodeWithValue<T>>(children_, value_);
-  }
+        // Override the Clone method to also clone the value.
+        //
+        // Note: if you want to convert `unique_ptr` into `shared_ptr`, you can use `std::shared_ptr<T>(std::move(ptr))`.
+        auto Clone() const -> std::unique_ptr<TrieNode> override {
+            return std::make_unique<TrieNodeWithValue<T>>(children_, value_);
+        }
 
-  // The value associated with this trie node.
-  std::shared_ptr<T> value_;
-};
+        // The value associated with this trie node.
+        std::shared_ptr<T> value_;
+    };
 
-// A Trie is a data structure that maps strings to values of type T. All operations on a Trie should not
-// modify the trie itself. It should reuse the existing nodes as much as possible, and create new nodes to
-// represent the new trie.
-class Trie {
- private:
-  // The root of the trie.
-  std::shared_ptr<const TrieNode> root_{nullptr};
+    // A Trie is a data structure that maps strings to values of type T. All operations on a Trie should not
+    // modify the trie itself. It should reuse the existing nodes as much as possible, and create new nodes to
+    // represent the new trie.
+    class Trie {
+    private:
+        // The root of the trie.
+        std::shared_ptr<const TrieNode> root_{nullptr};
 
-  // Create a new trie with the given root.
-  explicit Trie(std::shared_ptr<const TrieNode> root) : root_(std::move(root)) {}
+        // Create a new trie with the given root.
+        explicit Trie(std::shared_ptr<const TrieNode> root) : root_(std::move(root)) {}
 
- public:
-  // Create an empty trie.
-  Trie() = default;
+        template <class T>
+        auto Put(std::shared_ptr<const TrieNode> node, std::string_view key, int pos, T &value) const -> std::shared_ptr<const TrieNode> {
+            std::shared_ptr<TrieNode> new_node;
+            if (pos >= key.length()) {
+                if (!node) {
+                    new_node = std::make_shared<TrieNodeWithValue<T>>(std::make_shared<T>(std::move(value)));
+                } else {
+                    new_node = std::make_shared<TrieNodeWithValue<T>>(node->children_, std::make_shared<T>(std::move(value)));
+                }
+            } else {
+                if (!node) {
+                    new_node = std::make_shared<TrieNode>();
+                } else {
+                    new_node = std::move(node->Clone());
+                }
+                new_node->children_[key[pos]] = Put(new_node->children_[key[pos]], key, pos+1, value);
+            }
+            return new_node;
+        }
 
-  // Get the value associated with the given key.
-  // 1. If the key is not in the trie, return nullptr.
-  // 2. If the key is in the trie but the type is mismatched, return nullptr.
-  // 3. Otherwise, return the value.
-  template <class T>
-  auto Get(std::string_view key) const -> const T *;
+        // Check before used.
+        auto Remove(std::shared_ptr<const TrieNode> node, std::string_view key, int pos) const ->  std::shared_ptr<const TrieNode> {
+            std::shared_ptr<TrieNode> new_node;
+            if (pos >= key.length()) {
+                new_node = std::make_shared<TrieNode>(node->children_);
+            } else {
+                new_node = std::move(node->Clone());
+                new_node->children_[key[pos]] = Remove(new_node->children_[key[pos]], key, pos+1);
+            }
+            return new_node;
+        }
 
-  // Put a new key-value pair into the trie. If the key already exists, overwrite the value.
-  // Returns the new trie.
-  template <class T>
-  auto Put(std::string_view key, T value) const -> Trie;
+        bool Find(std::string_view key) const {
+            auto node = root_;
+            for (auto c : key) {
+                if (!node) return false;
+                auto it = node->children_.find(c);
+                if (it == node->children_.end()) return false;
+                node = it->second;
+            }
+            return node->is_value_node_;
+        }
 
-  // Remove the key from the trie. If the key does not exist, return the original trie.
-  // Otherwise, returns the new trie.
-  auto Remove(std::string_view key) const -> Trie;
-};
+    public:
+        // Create an empty trie.
+        Trie() = default;
 
+        // Get the value associated with the given key.
+        // 1. If the key is not in the trie, return nullptr.
+        // 2. If the key is in the trie but the type is mismatched, return nullptr.
+        // 3. Otherwise, return the value.
+        template <class T>
+        auto Get(std::string_view key) const -> const T * {
+            auto node = root_;
+            for (auto c : key) {
+                if (!node) return nullptr;
+                auto it = node->children_.find(c);
+                if (it == node->children_.end()) return nullptr;
+                node = it->second;
+            }
+            if (node->is_value_node_) {
+                return reinterpret_cast<const TrieNodeWithValue<T> *>(node.get())->value_.get();
+            } else
+                return nullptr;
+        }
 
-// This class is used to guard the value returned by the trie. It holds a reference to the root so
-// that the reference to the value will not be invalidated.
-template <class T>
-class ValueGuard {
- public:
-  ValueGuard(Trie root, const T &value) : root_(std::move(root)), value_(value) {}
-  auto operator*() const -> const T & { return value_; }
+        // Put a new key-value pair into the trie. If the key already exists, overwrite the value.
+        // Returns the new trie.
+        template <class T>
+        auto Put(std::string_view key, T value) const -> Trie {
+            return Trie(Put<T>(root_, key, 0, value));
+        }
 
- private:
-  Trie root_;
-  const T &value_;
-};
+        // Remove the key from the trie. If the key does not exist, return the original trie.
+        // Otherwise, returns the new trie.
+        auto Remove(std::string_view key) const -> Trie {
+            if (!Find(key)) return *this;
+            else return Trie(Remove(root_, key, 0));
+        }
+    };
 
-// This class is a thread-safe wrapper around the Trie class. It provides a simple interface for
-// accessing the trie. It should allow concurrent reads and a single write operation at the same
-// time.
-class TrieStore {
- public:
-  // This function returns a ValueGuard object that holds a reference to the value in the trie of the given version (default: newest version). If
-  // the key does not exist in the trie, it will return std::nullopt.
-  template <class T>
-  auto Get(std::string_view key, size_t version = -1) -> std::optional<ValueGuard<T>>;
+    // This class is used to guard the value returned by the trie. It holds a reference to the root so
+    // that the reference to the value will not be invalidated.
+    template <class T>
+    class ValueGuard {
+    public:
+        ValueGuard(Trie root, const T &value) : root_(std::move(root)), value_(value) {}
+        auto operator*() const -> const T & { return value_; }
 
-  // This function will insert the key-value pair into the trie. If the key already exists in the
-  // trie, it will overwrite the value
-  // return the version number after operation
-  // Hint: new version should only be visible after the operation is committed(completed)
-  template <class T>
-  size_t Put(std::string_view key, T value);
+    private:
+        Trie root_;
+        const T &value_;
+    };
 
-  // This function will remove the key-value pair from the trie.
-  // return the version number after operation
-  // if the key does not exist, version number should not be increased
-  size_t Remove(std::string_view key);
+    // This class is a thread-safe wrapper around the Trie class. It provides a simple interface for
+    // accessing the trie. It should allow concurrent reads and a single write operation at the same
+    // time.
+    class TrieStore {
+    public:
+        // This function returns a ValueGuard object that holds a reference to the value in the trie of the given version (default: newest version). If
+        // the key does not exist in the trie, it will return std::nullopt.
+        template <class T>
+        auto Get(std::string_view key, size_t version = -1) -> std::optional<ValueGuard<T>>;
 
-  // This function return the newest version number
-  size_t get_version();
+        // This function will insert the key-value pair into the trie. If the key already exists in the
+        // trie, it will overwrite the value
+        // return the version number after operation
+        // Hint: new version should only be visible after the operation is committed(completed)
+        template <class T>
+        size_t Put(std::string_view key, T value);
 
- private:
+        // This function will remove the key-value pair from the trie.
+        // return the version number after operation
+        // if the key does not exist, version number should not be increased
+        size_t Remove(std::string_view key);
 
-  // This mutex sequences all writes operations and allows only one write operation at a time.
-  // Concurrent modifications should have the effect of applying them in some sequential order
-  std::mutex write_lock_;
+        // This function return the newest version number
+        size_t get_version();
 
-  // Stores all historical versions of trie
-  // version number ranges from [0, snapshots_.size())
-  std::vector<Trie> snapshots_{1};
-};
+    private:
+        // This mutex sequences all writes operations and allows only one write operation at a time.
+        // Concurrent modifications should have the effect of applying them in some sequential order
+        std::mutex write_lock_;
+
+        // Stores all historical versions of trie
+        // version number ranges from [0, snapshots_.size())
+        std::vector<Trie> snapshots_{1};
+    };
 
 }  // namespace sjtu
 
